cd1cb20f429f26d38d5d24c845d6f2a30fbc1e99
Sage Price <sageeprice@gmail.com>
2019-02-04 20:26:53 -0500

    Updating intake lambda to handle updates with no note
    Enables giving a response that only has a rating, without any additional context. Sometimes I just want to give a number.

--- src/lambda/lambda_function.py
+++ src/lambda/lambda_function.py
@@ -5,11 +5,9 @@ from datetime import datetime
 def lambda_handler(event, context):
   """
   Reads SNS notification and writes message to DynamoDB.
-
   Args:
       event: SNS notification containing a SMS message.
       context: AWS execution specific metadata.
-
   Returns:
       HTTP response w/ status code 200 on completion.
   """
@@ -20,24 +18,31 @@ def lambda_handler(event, context):
   print('Parsed message from notification:' + json.dumps(payload))
   message_parts = payload['messageBody'].split("\n")
   print(message_parts)
-  note = ''
-  if (len(message_parts) > 1):
-    note = message_parts[1]
+
   mood_rating = message_parts[0]
+  item = {
+    'MessageId':{'S': payload['inboundMessageId']},
+    'ProcessedTimestamp':{'S': str(datetime.utcnow())},
+    'MoodRating': {'N': mood_rating},
+    'UserPhone': {'S': payload['originationNumber']}
+    }
+
+  # Add note if the message contains one.
+  if (len(message_parts) > 1):
+    note = ' '.join(message_parts[1:]).strip()
+    if note != '':
+      item['Note'] = {'S': note}
+
 
   dynamodb = boto3.client('dynamodb')
+  
+  print('Storing new mood: ' + json.dumps(item))
   dynamodb.put_item(
       TableName='MoodMessages',
-      Item={
-          'MessageId': {'S': payload['inboundMessageId']},
-          'ProcessedTimestamp': {'S': str(datetime.utcnow())},
-          'MoodRating': {'N': mood_rating},
-          'Note': {'S': note},
-          'UserPhone': {'S': payload['originationNumber']}
-          }
+      Item=item
       )
   
   return {
-      'statusCode': 200,
-      'body': json.dumps(event)
-  }
+    'statusCode': 200,
+    'body': json.dumps(event)
+    }
