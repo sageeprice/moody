f05230c7475a845bfbf6559c52aaaf09bb037418
sageeprice <sageeprice@gmail.com>
2019-10-02 21:37:01 -0400

    Cleaning up the vue.config.js pages

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -2,18 +2,21 @@
 
 // Reference: https://github.com/vuejs/vue-cli/blob/dev/docs/config/README.md
 module.exports = {
-  // publicPath: '/',
 
   pages: {
     'index': {
       entry: 'src/pages/index/main.js',
       template: 'public/index.html',
+      // when using title option, template title tag needs to be 
+      // <title><%= htmlWebpackPlugin.options.title 
       title: 'Home',
       chunks: ['index'],
     },
     moody: {
       entry: 'src/pages/moody/main.js',
       template: 'public/index.html',
+      // When using title option, template title tag needs to be 
+      // <title><%= htmlWebpackPlugin.options.title 
       title: 'Moody',
       chunks: ['moody'],
     },
